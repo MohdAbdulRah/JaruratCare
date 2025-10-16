3ee126c9d4a1b225e37daac4ae43a042cc71af8d
MohdAbdulRah <mohdnawaz5421@gmail.com>
2025-10-16 18:48:38 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import './App.css'
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link,Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Patients from './pages/Patients';
 import About from './pages/About';
@@ -12,7 +12,7 @@ return (
 <header className="bg-blue-600 text-white p-4 shadow-md flex justify-between items-center">
 <h1 className="text-2xl font-bold">Jarurat Care</h1>
 <nav className="space-x-4">
-<Link to="/" className="hover:underline">Home</Link>
+<Link to="/home" className="hover:underline">Home</Link>
 <Link to="/patients" className="hover:underline">Patients</Link>
 <Link to="/about" className="hover:underline">About</Link>
 </nav>
@@ -21,10 +21,16 @@ return (
 
 <main className="flex-grow p-6">
 <Routes>
-<Route path="/" element={<Home />} />
-<Route path="/patients" element={<Patients />} />
-<Route path="/about" element={<About />} />
-</Routes>
+      {/* Default route redirects to /patients */}
+      <Route path="/" element={<Navigate to="/patients" replace />} />
+
+      <Route path="/patients" element={<Patients />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+
+      {/* Fallback route for unmatched URLs */}
+      <Route path="*" element={<Navigate to="/patients" replace />} />
+    </Routes>
 </main>
 
 
